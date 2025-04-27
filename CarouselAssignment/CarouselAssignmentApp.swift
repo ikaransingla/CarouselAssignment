0a96dfb0fc5c7004fb6f8826a94abc128aadf6b8
ikaransingla <email.karan.23@gmail.com>
2025-04-27 16:33:52 +0530

    First Commit

---
+++ CarouselAssignment/CarouselAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselAssignmentApp.swift
+//  CarouselAssignment
+//
+//  Created by Karan Singla on 27/04/25.
+//
+
+import SwiftUI
+
+@main
+struct CarouselAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
